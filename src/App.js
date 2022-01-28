5a33f283c6e6a355f7a8fda75df1845fb69a98f2
Reinoptland <reinoptland@gmail.com>
2022-01-28 10:05:55 +0100

    Make productCard component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ProductCard from "./ProductCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ProductCard productName="Apple" price={1} />
+        <ProductCard productName="Banana" price={2} />
       </header>
     </div>
   );
