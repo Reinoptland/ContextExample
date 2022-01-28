1b02b66352c4344b879cc274e5161f5a816492c9
Reinoptland <reinoptland@gmail.com>
2022-01-28 10:55:32 +0100

    Add shoppingcart state to App, use callback props to buy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ProductCard from "./ProductCard";
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <ProductCard productName="Apple" price={1} />
-        <ProductCard productName="Banana" price={2} />
+        <ProductCard
+          productName="Apple"
+          price={1}
+          cart={cart}
+          setCart={setCart}
+        />
+        <ProductCard
+          productName="Banana"
+          price={2}
+          cart={cart}
+          setCart={setCart}
+        />
+        <ProductCard
+          productName="Lemon"
+          price={1.5}
+          cart={cart}
+          setCart={setCart}
+        />
       </header>
     </div>
   );
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,30 +1,21 @@
 import React from "react";
 
 export default function ProductCard(props) {
-  console.log("PROPS:", props);
   return (
     <div style={{ width: "300px" }}>
       <img src={`https://source.unsplash.com/200x200/?${props.productName}`} />
       <h1 className="big">{props.productName}</h1>
       <p>{props.productDescription}</p>
-      <button>Buy now €{props.price}</button>
+      <button
+        onClick={() =>
+          props.setCart([
+            ...props.cart,
+            { productName: props.productName, price: props.price },
+          ])
+        }
+      >
+        Buy now €{props.price}
+      </button>
     </div>
   );
 }
-
-// export default function ProductCard({
-//   productName,
-//   price,
-//   productDescription,
-//   ...props
-// }) {
-//   console.log("PROPS:", productName, price, productDescription);
-//   return (
-//     <div style={{ width: "300px" }}>
-//       <img src={`https://source.unsplash.com/200x200/?${productName}`} />
-//       <h1>{productName}</h1>
-//       <p>{productDescription}</p>
-//       <button>Buy now €{price}</button>
-//     </div>
-//   );
-// }
