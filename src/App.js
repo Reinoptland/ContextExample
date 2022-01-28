6b23e195b58c77c25e74cc68cae675e3d4277b0d
Reinoptland <reinoptland@gmail.com>
2022-01-28 11:03:28 +0100

    Create cart context, provide cart and setCart to the rest of App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ProductCard from "./ProductCard";
 
+const ShoppingCartContext = createContext({});
+
+// console.log("CONTEXT?", ShoppingCartContext);
+
 function App() {
   const [cart, setCart] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <ProductCard
-          productName="Apple"
-          price={1}
-          cart={cart}
-          setCart={setCart}
-        />
-        <ProductCard
-          productName="Banana"
-          price={2}
-          cart={cart}
-          setCart={setCart}
-        />
-        <ProductCard
-          productName="Lemon"
-          price={1.5}
-          cart={cart}
-          setCart={setCart}
-        />
-      </header>
-    </div>
+    <ShoppingCartContext.Provider value={{ cart: cart, setCart: setCart }}>
+      <div className="App">
+        <header className="App-header">
+          <ProductCard
+            productName="Apple"
+            price={1}
+            cart={cart}
+            setCart={setCart}
+          />
+          <ProductCard
+            productName="Banana"
+            price={2}
+            cart={cart}
+            setCart={setCart}
+          />
+          <ProductCard
+            productName="Lemon"
+            price={1.5}
+            cart={cart}
+            setCart={setCart}
+          />
+        </header>
+      </div>
+    </ShoppingCartContext.Provider>
   );
 }
 
